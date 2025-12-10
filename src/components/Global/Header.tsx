56cef53dd4f634eacd76f9b1100f5fae3e918f36
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2025-12-10 18:21:36 +0530

    pass GET_TAB_INFO data into Header Global component

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -1,30 +1,46 @@
-import { cn } from '@/lib/utils'
-import logo from '@/assets/logo.png'
+import {type TabInfo} from '_bg/getTabData'
+import {BOX_STYLES} from '~/Global/Container'
+
+import {useEffect, useState} from 'react'
+import {cn} from '@/lib/utils'
+
 import Button from '~/UI/Button'
 
 export default function Header() {
+  const [tabData, setTabData] = useState<TabInfo>({
+    favicon: null,
+    url: '',
+    title: '',
+  })
+
+  useEffect(() => {
+    chrome.runtime.sendMessage({type: 'GET_TAB_INFO'}, (response) => {
+      setTabData(response)
+    })
+  }, [])
+
   return (
-    <div className="flex items-center px-3.5 py-3 border-b-2 border-control">
+    <div className={cn(BOX_STYLES, 'py-3 flex justify-between border-b-3 border-control')}>
+      <div className={cn('size-10 bg-control rounded-lg p-2', 'group')}>
+        <div className={cn('size-full bg-white rounded-full', 'group-hover:scale-[1.1] group-hover:bg-white/80 duration-300')}></div>
+      </div>
 
       <div className="flex items-center gap-2">
-        <div className={cn('size-10 rounded-full overflow-hidden', 'group')}>
-          <img
-            src={logo}
-            alt="logo"
-            className={cn(
-              'size-full object-cover',
-              'group-hover:scale-105 duration-300'
-            )}
-          />
+        <div className="size-5 flex items-center justify-center rounded-sm bg-gray-200">
+          {tabData.favicon ? (
+            <img src={tabData.favicon} alt="Favicon" className="size-full rounded-sm" />
+          ) : (
+            <span className="text-xs text-gray-500">🌐</span> // Placeholder icon
+          )}
         </div>
 
-        <span className="text-white text-lg font-semibold tracking-wide">
-          ONKLIQ
-        </span>
-
-        <Button variant="secondary">Button</Button>
+        <div>
+          {tabData.title && <p className="text-sm font-medium">{tabData.title}</p>}
+          {tabData.url && <p className="text-xs text-gray-500">{tabData.url}</p>}
+        </div>
       </div>
-      
+
+      <Button variant="secondary">Button</Button>
     </div>
   )
 }
